71da56ba7cb85ffdd850517fc788e7c5f76ec6fa
Noor Alam <engineernooralam143@gmail.com>
2023-10-10 08:34:06 +0600

    update private route

--- src/main.jsx
+++ src/main.jsx
@@ -44,11 +44,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/profile",
-        element:<Profile></Profile> ,
+        element:<PrivateRoute><Profile></Profile></PrivateRoute> ,
       },
       {
         path: "/dashboard",
-        element: <Dashboard></Dashboard> ,
+        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute> ,
       },
       {
         path: "/login",
